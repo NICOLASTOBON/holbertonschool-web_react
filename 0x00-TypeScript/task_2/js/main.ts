10992fc4764bf1681e9dfadd4d7c08a08ba96307
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-06-22 15:10:12 -0500

    [ADD] class

---
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -0,0 +1,32 @@
+interface DirectorInterface {
+  workFromHome(): string,
+  getCoffeeBreak(): string,
+  workDirectorTasks(): string,
+}
+
+interface TeacherInterface {
+  workFromHome(): string,
+  getCoffeeBreak(): string,
+  workTeacherTasks(): string,
+}
+
+export class Director implements DirectorInterface {
+  workFromHome() { return 'Working from home'}
+  getCoffeeBreak() { return 'Getting a coffee break' }
+  workDirectorTasks() { return 'Getting to director tasks'}
+}
+
+export class Teacher implements TeacherInterface {
+  workFromHome() { return 'Cannot work from home'}
+  getCoffeeBreak() { return 'Cannot have a break' }
+  workTeacherTasks() { return 'Getting to work' }
+}
+
+export function createEmployee(salary: number | string): Director | Teacher {
+  salary = !Number.isInteger(salary) ? Number(salary) : salary
+
+  if (salary < 500) {
+    return new Teacher()
+  }
+  return new Director()
+}

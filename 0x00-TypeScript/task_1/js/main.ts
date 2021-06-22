67bb3506a22347186eff46bde5d8128eb0ddc880
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-06-22 13:29:42 -0500

    [ADD] class

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -11,10 +11,39 @@ interface Directors extends Teacher {
   numberOfReports: number,
 }
 
-interface printTeacherFunction {
+interface PrintTeacherFunction {
   (firstName: string, lastName: string): string,
 }
 
-export const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+interface StudentClassInteface {
+  firstName: string,
+  lastName: string,
+  workOnHomework(): string,
+  displayName(): string,
+}
+
+interface StudentConstructor {
+  new(firstName: string, lastName: string): StudentClassInteface,
+}
+
+export const printTeacher: PrintTeacherFunction = (firstName: string, lastName: string): string => {
   return `${firstName[0]}. ${lastName}`;
+}
+
+export const StudentClass: StudentConstructor =  class StudentClass implements StudentClassInteface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently workin";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
 }

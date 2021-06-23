4cae05b685a4092e2e44e38644f0e92a2e411fb8
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-06-22 19:56:52 -0500

    [ADD] Ambient Namespaces

--- 0x00-TypeScript/task_3/js/main.ts
+++ 0x00-TypeScript/task_3/js/main.ts
@@ -11,3 +11,7 @@ const newRowID: RowID = CRUD.insertRow(row);
 const updatedRow: RowElement = {...row, age: 23};
 CRUD.updateRow(newRowID, updatedRow);
 CRUD.deleteRow(newRowID);
+
+// ["./crud.d.ts />"]
+// ["/// <reference path =", "./crud.js"]
+// ["CRUD.updateRow(newRowID, updatedRow)", "CRUD.deleteRow(newRowID)"]

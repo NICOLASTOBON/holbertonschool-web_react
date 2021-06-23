e05f8d4c747ab957f9e4dbd395678ccbcd690565
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-06-22 22:22:39 -0500

    [ADD] main file

--- 0x00-TypeScript/task_4/js/main.ts
+++ 0x00-TypeScript/task_4/js/main.ts
@@ -28,3 +28,15 @@ console.log('React');
 react.setTeacher = cTeacher;
 console.log(react.getRequirements());
 console.log(react.getAvailableTeacher());
+
+// ["./subjects/Teacher.ts />"]
+// ["./subjects/Subject.ts />"]
+// ["./subjects/Cpp.ts />"]
+// ["./subjects/Java.ts />"]
+// ["./subjects/React.ts />"]
+
+// ["/// <reference path =", "./subjects/Teacher.ts"]
+// ["/// <reference path =", "./subjects/Subject.ts"]
+// ["/// <reference path =", "./subjects/Cpp.ts"]
+// ["/// <reference path =", "./subjects/Java.ts"]
+// ["/// <reference path =", "./subjects/React.ts"]

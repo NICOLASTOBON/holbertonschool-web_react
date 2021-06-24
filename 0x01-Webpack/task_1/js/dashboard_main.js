6bed18a6be70ae682d9e497aaf4072c146fb94ac
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-06-24 01:43:28 -0500

    [ADD] files

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -7,7 +7,7 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
-$('button').on('click', _.debounce(updateCounter));
+$('button').on('click', _.debounce(updateCounter, 500));
 
 function updateCounter() {
   const clicks = $('#count').html().split(" ", 1) + 1 || 1;

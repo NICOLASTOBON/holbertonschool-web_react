5db7f0884fa2c26c29ee9e08f3c57166769857b8
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-06-24 01:44:59 -0500

    [ADD] files

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,15 +1,16 @@
-import $ from 'jquery';
-import _ from 'lodash';
+const $ = require( "jquery" );
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append("<p id='count'></p>");
+$('body').append('<p id=\'count\'></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let count = 0;
+button.addEventListener("click", _.debounce(updateCounter));
+
 function updateCounter() {
-  $('#count').html(`${count++} clicks on the button`);
+  const clicks = $('#count').html().split(" ", 1) + 1 || 1;
+  $('#count').html(`${clicks} clicks on the button`);
 }
 
-$('button').on('click', _.debounce(updateCounter, 500));
+// "500"

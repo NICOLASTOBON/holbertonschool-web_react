2285bca7cdb6e566414c29acbd188aecc6e65037
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-06-24 00:19:56 -0500

    [ADD] files

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -1,5 +1,5 @@
 import $ from "jquery";
 
-$('<p>').text('Holberton Dashboard').appendTo('body');
-$('<p>').text('Dashboard data for the students').appendTo('body');
-$('<p>').text('Copyright - Holberton School').appendTo('body');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');

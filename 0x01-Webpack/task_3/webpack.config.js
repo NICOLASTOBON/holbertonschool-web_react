e126a46f483ce4531c2c1882786cea490cfccfe8
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-06-25 03:29:37 -0500

    [ADD] files

--- 0x01-Webpack/task_3/webpack.config.js
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -25,7 +26,10 @@ module.exports = {
       },
     ],
   },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new CleanWebpackPlugin(),
+  ],
   devtool: 'inline-source-map',
   optimization: {
     splitChunks: {

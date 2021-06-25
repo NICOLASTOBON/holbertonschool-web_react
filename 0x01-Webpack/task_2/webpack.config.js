3bc7549af8bf153902c9472b3475c392ce0ae5e4
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-06-24 23:42:59 -0500

    [ADD] files

--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -1,31 +1,22 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './js/dashboard_main.js',
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    mode: "production",
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(gif|png|jpe?g|svg)$/i,
-                use: [
-                  'file-loader',
-                  {
-                    loader: 'image-webpack-loader',
-                    options: {
-                      bypassOnDebug: true, // webpack@1.x
-                      disable: true, // webpack@2.x and newer
-                    },
-                  },
-                ],
-            }
-        ],
-    },
+  entry: "./js/dashboard_main.js",
+  mode: "production",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/,
+        use: ['file-loader', 'image-webpack-loader'],
+      },
+    ],
+  },
 };

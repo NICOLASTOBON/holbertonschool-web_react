3e7a2dc474ad028c2ccc458b4382ea11578d2358
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-06-25 03:26:14 -0500

    [ADD] files

---
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    header: './modules/header/header.js',
+    body: './modules/body/body.js',
+    footer: './modules/footer/footer.js',
+  },
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].bundle.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/,
+        use: ['file-loader', 'image-webpack-loader'],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  devServer: {
+    contentBase: './public',
+    compress: true,
+    port: 8564,
+  }
+};

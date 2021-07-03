41740a01891401fd2a21e0d48264346d0262fa5c
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-07-03 01:42:47 -0500

    [Add] all files

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,22 +1,31 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  entry: "../src/index.js",
+  mode: 'development',
+  entry: ['./src/index.js'],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.js',
+    clean: true
   },
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.css$/,
-        user: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(gif|png|jpe?g|svg)$/,
-        use: ['file-loader', 'image-webpack-loader']
-      }
-    ]
+        use: ['file-loader', 'image-webpack-loader'],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '../dist'),
+    hot: true,
+    compress: true,
+    port: 8564
   },
   devtool: 'inline-source-map'
-}
+};

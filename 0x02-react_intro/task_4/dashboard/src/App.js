99dd5ca92a816ce4521487c1cdab091f2392c209
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-07-02 23:15:06 -0500

    [Add] all files

---
+++ 0x02-react_intro/task_4/dashboard/src/App.js
@@ -0,0 +1,28 @@
+import logo from './dashbord/logo.jpg'
+
+import { getFullYear, getFooterCopy } from './utils.js'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <img src={logo} alt="Holberton Logo"/>
+        <h1>School dashboard</h1>
+      </header>
+      <main className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email"/>
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password"/>
+        <button type="button">OK</button>
+      </main>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;

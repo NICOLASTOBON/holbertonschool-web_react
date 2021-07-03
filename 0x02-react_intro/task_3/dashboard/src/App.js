c55ba870578543643598334ec0ab8bb9f66c834c
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-07-03 15:29:21 -0500

    [Add] all files

--- 0x02-react_intro/task_3/dashboard/src/App.js
+++ 0x02-react_intro/task_3/dashboard/src/App.js
@@ -12,9 +12,9 @@ function App() {
       </header>
       <main className="App-body">
         <p>Login to access the full dashboard</p>
-        <label for="email">Email:</label>
+        <label htmlFor="email">Email:</label>
         <input type="email" name="email" id="email"/>
-        <label for="password">Password:</label>
+        <label htmlFor="password">Password:</label>
         <input type="password" name="password" id="password"/>
         <button type="button">OK</button>
       </main>
--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  entry: '../src/index.js',
+  context: path.resolve(__dirname, '../src/'),
+  entry: './index.js',
   output: {
     path: path.resolve(__dirname, '../dist/'),
     filename: 'bundle.js',
+    clean: true,
+  },
+  performance: {
+    maxAssetSize: 100000,
   },
   module: {
     rules: [
@@ -16,7 +21,19 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         use: ['file-loader', 'image-webpack-loader']
       },
+      {
+        test: /\.(js)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/react']
+          }
+        }
+      }
     ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
   },
   devServer: {
     contentBase: path.resolve(__dirname, '../dist/'),
